476fcd70efe37c23b10e33717eed5d97c61243dc
Martin Cross <mncross@gmail.com>
2015-08-26 20:24:52 -0500

    edit modules to only return value and not edit DOM

--- cheeses.js
+++ cheeses.js
@@ -1,11 +1,11 @@
 var sandwich = (function(sandwich){
 	var cheeses = {
-		"Muenster": "1.00",
-		"Cheddar": "1.00",
-		"Provolone": "1.00",
-		"American": "1.00",
-		"Swiss": "1.00",
-		"Pepperjack": "1.00"
+		"Muenster": 1.00,
+		"Cheddar": 1.00,
+		"Provolone": 1.00,
+		"American": 1.00,
+		"Swiss": 1.00,
+		"Pepperjack": 1.00
 	};
 
 	sandwich.getCheeses = function() {
@@ -13,14 +13,8 @@ var sandwich = (function(sandwich){
 		return Object.keys(cheeses);
 	};
 
-	sandwich.addCheese = function() {
-		//get value of cheese drop down
-		console.log("addCheese run");
-		var selection = $("#cheeses").val();
-		console.log(selection);
-		//append value to #sandwich
-		$("#cheese-type").append(selection);
-		$("#cheese-price").append(cheeses[selection]);
+	sandwich.addCheese = function(cheeseChosen) {
+		return cheeses[cheeseChosen];
 	};
 
 	return sandwich;
