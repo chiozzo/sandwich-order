ea198d94c500ae1f2d1bef35b51ef54f8d6ba3d3
Martin Cross <mncross@gmail.com>
2015-08-26 19:53:26 -0500

    added buttons for each ingredient

--- sandwich.js
+++ sandwich.js
@@ -4,35 +4,40 @@ var condiments = sandwich.getCondiments();
 var meats = sandwich.getMeats();
 var veggies = sandwich.getVeggies();
 
-var menuBread = $('<select id="breads">').appendTo('#ingredients');
-var menuCheese = $('<select id="cheeses">').appendTo('#ingredients');
-var menuCondiment = $('<select id="condiments">').appendTo('#ingredients');
-var menuMeat = $('<select id="meats">').appendTo('#ingredients');
-var menuVeggie = $('<select id="veggies">').appendTo('#ingredients');
+var menuBread = $('<select id="breads">').appendTo('#bread');
+var menuCheese = $('<select id="cheeses">').appendTo('#cheese');
+var menuCondiment = $('<select id="condiments">').appendTo('#condiment');
+var menuMeat = $('<select id="meats">').appendTo('#meat');
+var menuVeggie = $('<select id="veggies">').appendTo('#veggie');
 
 menuBread.append("<option selected disabled>Choose Your Bread</option>");
 $(breads).each(function() {
   menuBread.append($("<option>").attr('value',this).text(this));
 });
+$("<button id='bread-button'>Add Bread</button>").appendTo("#bread");
 menuCheese.append("<option selected disabled>Choose Your Cheese</option>");
 $(cheeses).each(function() {
   menuCheese.append($("<option>").attr('value',this).text(this));
 });
+$("<button id='cheese-button'>Add Cheese</button>").appendTo("#cheese");
 menuCondiment.append("<option selected disabled>Choose Your Condiment</option>");
 $(condiments).each(function() {
   menuCondiment.append($("<option>").attr('value',this).text(this));
 });
+$("<button id='condiment-button'>Add Condiment</button>").appendTo("#condiment");
 menuMeat.append("<option selected disabled>Choose Your Meat</option>");
 $(meats).each(function() {
   menuMeat.append($("<option>").attr('value',this).text(this));
 });
+$("<button id='meat-button'>Add Meat</button>").appendTo("#meat");
 menuVeggie.append("<option selected disabled>Choose Your Veggie</option>");
 $(veggies).each(function() {
   menuVeggie.append($("<option>").attr('value',this).text(this));
 });
+$("<button id='veggie-button'>Add Veggie</button>").appendTo("#veggie");
 
-$("#sandwich-build").on("click", sandwich.addBread);
-$("#sandwich-build").on("click", sandwich.addCheese);
-$("#sandwich-build").on("click", sandwich.addCondiment);
-$("#sandwich-build").on("click", sandwich.addMeat);
-$("#sandwich-build").on("click", sandwich.addVeggie);
+$("#bread-button").on("click", sandwich.addBread);
+$("#meat-button").on("click", sandwich.addCheese);
+$("#condiment-button").on("click", sandwich.addCondiment);
+$("#meat-button").on("click", sandwich.addMeat);
+$("#veggie-button").on("click", sandwich.addVeggie);
